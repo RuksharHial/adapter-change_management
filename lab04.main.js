1e0c1860979591fb41bad62653cceda9a132ad82
RuksharHial <ruksharhial1997@gmail.com>
2021-08-27 06:57:30 +0000

    Lab 4

--- lab04.main.js
+++ lab04.main.js
@@ -1,287 +1,45 @@
+// Update this section with your ServiceNow credentials
+const options = {
+  url: 'https://dev80062.service-now.com/',
+  username: 'admin',
+  password: '2ayAZhZzzTK6',
+  serviceNowTable: 'change_request'
+};
 // Import built-in Node.js package path.
 const path = require('path');
 
 /**
- * Import the ServiceNowConnector class from local Node.js module connector.js
+ * Import the ServiceNowConnector class from local Node.js module connector.js.
  *   and assign it to constant ServiceNowConnector.
  * When importing local modules, IAP requires an absolute file reference.
  * Built-in module path's join method constructs the absolute filename.
  */
-const ServiceNowConnector = require(path.join(__dirname, '/connector.js'));
+const ServiceNowConnector = require(path.join(__dirname, './connector.js'));
 
 /**
- * Import built-in Node.js package events' EventEmitter class and
- * assign it to constant EventEmitter. We will create a child class
- * from this class.
+ * @function mainOnObject
+ * @description Instantiates an object from the imported ServiceNowConnector class
+ *   and tests the object's get and post methods.
  */
-const EventEmitter = require('events').EventEmitter;
+function mainOnObject() {
+  // Instantiate an object from class ServiceNowConnector.
+  const connector = new ServiceNowConnector(options);
+  // Test the object's get and post methods.
+  // You must write the arguments for get and post.
+  connector.get((data, error) => {
+    if (error) {
+      console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
+    }
+    console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`)
+  });
+  connector.post((data, error) => {
+    if (error) {
+      console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
+    }
+    console.log(`\nResponse returned from POST request:\n${JSON.stringify(data)}`)
+  });
 
-/**
- * The ServiceNowAdapter class.
- *
- * @summary ServiceNow Change Request Adapter
- * @description This class contains IAP adapter properties and methods that IAP
- *   brokers and products can execute. This class inherits the EventEmitter
- *   class.
- */
-class ServiceNowAdapter extends EventEmitter {
-
-  /**
-   * Here we document the ServiceNowAdapter class' callback. It must follow IAP's
-   *   data-first convention.
-   * @callback ServiceNowAdapter~requestCallback
-   * @param {(object|string)} responseData - The entire REST API response.
-   * @param {error} [errorMessage] - An error thrown by REST API call.
-   */
-
-  /**
-   * Here we document the adapter properties.
-   * @typedef {object} ServiceNowAdapter~adapterProperties - Adapter
-   *   instance's properties object.
-   * @property {string} url - ServiceNow instance URL.
-   * @property {object} auth - ServiceNow instance credentials.
-   * @property {string} auth.username - Login username.
-   * @property {string} auth.password - Login password.
-   * @property {string} serviceNowTable - The change request table name.
-   */
-
-  /**
-   * @memberof ServiceNowAdapter
-   * @constructs
-   *
-   * @description Instantiates a new instance of the Itential ServiceNow Adapter.
-   * @param {string} id - Adapter instance's ID.
-   * @param {ServiceNowAdapter~adapterProperties} adapterProperties - Adapter instance's properties object.
-   */
-  constructor(id, adapterProperties) {
-    // Call super or parent class' constructor.
-    super();
-    // Copy arguments' values to object properties.
-    this.id = id;
-    this.props = adapterProperties;
-    // Instantiate an object from the connector.js module and assign it to an object property.
-    this.connector = new ServiceNowConnector({
-      url: this.props.url,
-      username: this.props.auth.username,
-      password: this.props.auth.password,
-      serviceNowTable: this.props.serviceNowTable
-    });
-  }
-
-  /**
-   * @memberof ServiceNowAdapter
-   * @method connect
-   * @summary Connect to ServiceNow
-   * @description Complete a single healthcheck and emit ONLINE or OFFLINE.
-   *   IAP calls this method after instantiating an object from the class.
-   *   There is no need for parameters because all connection details
-   *   were passed to the object's constructor and assigned to object property this.props.
-   */
-  connect() {
-    // As a best practice, Itential recommends isolating the health check action
-    // in its own method.
-    this.healthcheck();
-  }
-
-  /**
- * @memberof ServiceNowAdapter
- * @method healthcheck
- * @summary Check ServiceNow Health
- * @description Verifies external system is available and healthy.
- *   Calls method emitOnline if external system is available.
- *
- * @param {ServiceNowAdapter~requestCallback} [callback] - The optional callback
- *   that handles the response.
- */
-healthcheck(callback) {
- this.getRecord((result, error) => {
-   /**
-    * For this lab, complete the if else conditional
-    * statements that check if an error exists
-    * or the instance was hibernating. You must write
-    * the blocks for each branch.
-    */
-    let errorMessage = null;
-    let responseData = null;
-
-   if (error) {
-     /**
-      * Write this block.
-      * If an error was returned, we need to emit OFFLINE.
-      * Log the returned error using IAP's global log object
-      * at an error severity. In the log message, record
-      * this.id so an administrator will know which ServiceNow
-      * adapter instance wrote the log message in case more
-      * than one instance is configured.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing the error seen as an argument
-      * for the callback's errorMessage parameter.
-      */
-     // log.error(`Error from ${this.id}  : ${error}`);
-      this.emitOffline();
-        errorMessage = error;
-   } else {
-     /**
-      * Write this block.
-      * If no runtime problems were detected, emit ONLINE.
-      * Log an appropriate message using IAP's global log object
-      * at a debug severity.
-      * If an optional IAP callback function was passed to
-      * healthcheck(), execute it passing this function's result
-      * parameter as an argument for the callback function's
-      * responseData parameter.
-      */
-    //  log.debug(result);
-      this.emitOnline();
-        responseData = result;
-      
-   }
-   if(callback){
-       return callback(responseData,errorMessage);
-   }
- });
 }
 
-  /**
-   * @memberof ServiceNowAdapter
-   * @method emitOffline
-   * @summary Emit OFFLINE
-   * @description Emits an OFFLINE event to IAP indicating the external
-   *   system is not available.
-   */
-  emitOffline() {
-    this.emitStatus('OFFLINE');
-  //  log.warn('ServiceNow: Instance is unavailable.');
-  }
-
-  /**
-   * @memberof ServiceNowAdapter
-   * @method emitOnline
-   * @summary Emit ONLINE
-   * @description Emits an ONLINE event to IAP indicating external
-   *   system is available.
-   */
-  emitOnline() {
-    this.emitStatus('ONLINE');
-   // log.info('ServiceNow: Instance is available.');
-  }
-
-  /**
-   * @memberof ServiceNowAdapter
-   * @method emitStatus
-   * @summary Emit an Event
-   * @description Calls inherited emit method. IAP requires the event
-   *   and an object identifying the adapter instance.
-   *
-   * @param {string} status - The event to emit.
-   */
-  emitStatus(status) {
-    this.emit(status, { id: this.id });
-  }
-
-  /**
-   * @memberof ServiceNowAdapter
-   * @method getRecord
-   * @summary Get ServiceNow Record
-   * @description Retrieves a record from ServiceNow.
-   *
-   * @param {ServiceNowAdapter~requestCallback} callback - The callback that
-   *   handles the response.
-   */
-  getRecord(callback) {
-    /**
-     * Write the body for this function.
-     * The function is a wrapper for this.connector's get() method.
-     * Note how the object was instantiated in the constructor().
-     * get() takes a callback function.
-     */
-     this.connector.get((data,error)=>{
-
-         let callbackResult = null;
-         let callbackError = null;
-
-         if(error){
-             callbackError=error.body;
-         }
-         if(data && data.body){
-             let newElement = [];
-             let recordSet = JSON.parse(data.body).result;
-             recordSet.forEach((value,index)=>{
-                 newElement.push({
-                    change_ticket_number:value.number,
-                     active:value.active,
-                     priority:value.priority,
-                     description:value.description,
-                     work_start:value.work_start,
-                     work_end:value.work_end,
-                     change_ticket_key:value.sys_id
-                 })
-             })
-             callbackResult = newElement;
-         }
-
-         callback(callbackResult, callbackError)
-
-     });
-  }
-
-  /**
-   * @memberof ServiceNowAdapter
-   * @method postRecord
-   * @summary Create ServiceNow Record
-   * @description Creates a record in ServiceNow.
-   *
-   * @param {ServiceNowAdapter~requestCallback} callback - The callback that
-   *   handles the response.
-   */
-  postRecord(callback) {
-    /**
-     * Write the body for this function.
-     * The function is a wrapper for this.connector's post() method.
-     * Note how the object was instantiated in the constructor().
-     * post() takes a callback function.
-     */
-       this.connector.post((results, error) => {
-            let callBackResult = null;
-            let callBackResultError = null;
-            if(error){
-                callBackResultError = error.body;
-            }
-
-            if(results && results.body){
-                let recordSet = JSON.parse(results.body).result;
-                let newElement = {
-                        change_ticket_number:recordSet.number,
-                        active:recordSet.active,
-                        priority:recordSet.priority,
-                        description:recordSet.description,
-                        work_start:recordSet.work_start,
-                        work_end:recordSet.work_end,
-                        change_ticket_key:recordSet.sys_id
-                    }
-                
-                callBackResult = newElement;
-            }
-            
-            callback(callBackResult, callBackResultError)
-        });
-  }
-}
-
-module.exports = ServiceNowAdapter;
-
-var j = new module.exports('test',{
-  "url": "https://dev80062.service-now.com/",
-  "auth": {
-    "username": "admin",
-    "password": "2ayAZhZzzTK6"
-  },
-  "serviceNowTable": "change_request"
-});
-
-j.getRecord((results, error) =>{
-    
-    results?console.log('result Set : ', results):'';
-    error?console.log('error : ', error):'';
-
-});
+// Call mainOnObject to run it.
+mainOnObject();
